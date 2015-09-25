746fbe98f5de696cc00d19cbb6662126fc70f72b
felicepantaleo <felice.pantaleo@cern.ch>
2015-09-25 18:19:05 +0200

    compile fix

--- pingpong.cpp
+++ pingpong.cpp
@@ -48,16 +48,6 @@ int skip = 1000;
 int loop = 10000;
 
 
-
-void usage (void);
-int init_cuda_context (void);
-int destroy_cuda_context (void);
-int process_options (int argc, char *argv[]);
-int allocate_memory (char **sbuf, char **rbuf, int rank);
-void print_header (int rank);
-void touch_data (void *sbuf, void *rbuf, int rank, size_t size);
-void free_memory (void *sbuf, void *rbuf, int rank);
-
 int
 main (int argc, char *argv[])
 {
@@ -66,14 +56,7 @@ main (int argc, char *argv[])
     MPI_Status reqstat;
     char *s_buf, *r_buf;
     double t_start = 0.0, t_end = 0.0;
-    int po_ret = process_options(argc, argv);
 
-    if (po_okay == po_ret && cuda == options.accel) {
-        if (init_cuda_context()) {
-            fprintf(stderr, "Error initializing cuda context\n");
-            exit(EXIT_FAILURE);
-        }
-    }
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
@@ -90,7 +73,7 @@ main (int argc, char *argv[])
 
     /* Latency test */
     for(size = 0; size <= MAX_MSG_SIZE; size = (size ? size * 2 : 1)) {
-        touch_data(s_buf, r_buf, myid, size);
+
 
         if(size > LARGE_MESSAGE_SIZE) {
             loop = LOOP_LARGE;
@@ -130,222 +113,6 @@ main (int argc, char *argv[])
     MPI_Finalize();
 
    return EXIT_SUCCESS;
-}
-
-
-int
-allocate_device_buffer (char ** buffer)
-{
-#ifdef _ENABLE_CUDA_
-    cudaError_t cuerr = cudaSuccess;
-#endif
-
-    switch (options.accel) {
-#ifdef _ENABLE_CUDA_
-        case cuda:
-            cuerr = cudaMalloc((void **)buffer, MYBUFSIZE);
-
-            if (cudaSuccess != cuerr) {
-                fprintf(stderr, "Could not allocate device memory\n");
-                return 1;
-            }
-            break;
-#endif
-#ifdef _ENABLE_OPENACC_
-        case openacc:
-            *buffer = acc_malloc(MYBUFSIZE);
-            if (NULL == *buffer) {
-                fprintf(stderr, "Could not allocate device memory\n");
-                return 1;
-            }
-            break;
-#endif
-        default:
-            fprintf(stderr, "Could not allocate device memory\n");
-            return 1;
-    }
-
-    return 0;
-}
-
-void *
-align_buffer (void * ptr, unsigned long align_size)
-{
-    return (void *)(((unsigned long)ptr + (align_size - 1)) / align_size *
-            align_size);
-}
-
-int
-allocate_memory (char ** sbuf, char ** rbuf, int rank)
-{
-    unsigned long align_size = getpagesize();
-
-    assert(align_size <= MAX_ALIGNMENT);
-
-    switch (rank) {
-        case 0:
-            if ('D' == options.src) {
-                if (allocate_device_buffer(sbuf)) {
-                    fprintf(stderr, "Error allocating cuda memory\n");
-                    return 1;
-                }
-
-                if (allocate_device_buffer(rbuf)) {
-                    fprintf(stderr, "Error allocating cuda memory\n");
-                    return 1;
-                }
-            }
-
-            else {
-                *sbuf = align_buffer(s_buf_original, align_size);
-                *rbuf = align_buffer(r_buf_original, align_size);
-            }
-            break;
-        case 1:
-            if ('D' == options.dst) {
-                if (allocate_device_buffer(sbuf)) {
-                    fprintf(stderr, "Error allocating cuda memory\n");
-                    return 1;
-                }
-
-                if (allocate_device_buffer(rbuf)) {
-                    fprintf(stderr, "Error allocating cuda memory\n");
-                    return 1;
-                }
-            }
-
-            else {
-                *sbuf = align_buffer(s_buf_original, align_size);
-                *rbuf = align_buffer(r_buf_original, align_size);
-            }
-            break;
-    }
-
-    return 0;
-}
-
-void
-print_header (int rank)
-{
-    if (0 == rank) {
-        switch (options.accel) {
-            case cuda:
-                printf(HEADER, "-CUDA");
-                break;
-            case openacc:
-                printf(HEADER, "-OPENACC");
-                break;
-            default:
-                printf(HEADER, "");
-                break;
-        }
-
-        switch (options.accel) {
-            case cuda:
-            case openacc:
-                printf("# Send Buffer on %s and Receive Buffer on %s\n",
-                        'D' == options.src ? "DEVICE (D)" : "HOST (H)",
-                        'D' == options.dst ? "DEVICE (D)" : "HOST (H)");
-            default:
-                printf("%-*s%*s\n", 10, "# Size", FIELD_WIDTH, "Latency (us)");
-                fflush(stdout);
-        }
-    }
-}
-
-void
-set_device_memory (void * ptr, int data, size_t size)
-{
-#ifdef _ENABLE_OPENACC_
-    size_t i;
-    char * p = (char *)ptr;
-#endif
-
-    switch (options.accel) {
-#ifdef _ENABLE_CUDA_
-        case cuda:
-            cudaMemset(ptr, data, size);
-            break;
-#endif
-#ifdef _ENABLE_OPENACC_
-        case openacc:
-#pragma acc parallel loop deviceptr(p)
-            for(i = 0; i < size; i++) {
-                p[i] = data;
-            }
-            break;
-#endif
-        default:
-            break;
-    }
-}
-
-void
-touch_data (void * sbuf, void * rbuf, int rank, size_t size)
-{
-    if ((0 == rank && 'H' == options.src) ||
-            (1 == rank && 'H' == options.dst)) {
-        memset(sbuf, 'a', size);
-        memset(rbuf, 'b', size);
-    } else {
-        set_device_memory(sbuf, 'a', size);
-        set_device_memory(rbuf, 'b', size);
-    }
-}
-
-int
-free_device_buffer (void * buf)
-{
-    switch (options.accel) {
-#ifdef _ENABLE_CUDA_
-        case cuda:
-            cudaFree(buf);
-            break;
-#endif
-#ifdef _ENABLE_OPENACC_
-        case openacc:
-            acc_free(buf);
-            break;
-#endif
-        default:
-            /* unknown device */
-            return 1;
-    }
-
-    return 0;
-}
-
-int
-destroy_cuda_context (void)
-{
-#ifdef _ENABLE_CUDA_
-    CUresult curesult = CUDA_SUCCESS;
-    curesult = cuCtxDestroy(cuContext);
-
-    if (curesult != CUDA_SUCCESS) {
-        return 1;
-    }
-#endif
-    return 0;
-}
-
-void
-free_memory (void * sbuf, void * rbuf, int rank)
-{
-    switch (rank) {
-        case 0:
-            if ('D' == options.src) {
-                free_device_buffer(sbuf);
-                free_device_buffer(rbuf);
-            }
-            break;
-        case 1:
-            if ('D' == options.dst) {
-                free_device_buffer(sbuf);
-                free_device_buffer(rbuf);
-            }
-            break;
-    }
 }
 
 /* vi:set sw=4 sts=4 tw=80: */
